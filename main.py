509856f0089e9d9a260bae01e2c032d95c8e0127
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 17:41:25 +0200

    feat: define add_time function and print all exemples provided by fcc

---
+++ main.py
@@ -0,0 +1,16 @@
+
+def add_time():
+    pass
+
+# Examples
+print(add_time("3:00 PM", "3:10"))
+print(add_time("11:30 AM", "2:32", "Monday"))
+print(add_time("11:43 AM", "00:20"))
+print(add_time("10:10 PM", "3:30"))
+print(add_time("11:43 PM", "24:20", "tueSday"))
+print(add_time("6:30 PM", "205:12"))
+
+print(add_time("11:06 PM", "2:02"))
+print(add_time("11:59 PM", "24:05"))
+print(add_time("2:59 AM", "24:00"))
+print(add_time("8:16 PM", "466:02"))
